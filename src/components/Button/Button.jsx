0a16ff76e1cbcb73a35c4d6e04969a40162d9cd0
Yehor Hryhortsev <egor.goldenrash@gmail.com>
2023-12-11 21:13:08 +0200

    fix click load more

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,9 +2,5 @@ import React from 'react';
 import { ButtonStyled } from './Button.styled';
 
 export const Button = ({ getPage }) => {
-  const incrementPage = () => {
-    getPage();
-  };
-
-  return <ButtonStyled onClick={incrementPage}>Load more</ButtonStyled>;
+  return <ButtonStyled onClick={getPage}>Load more</ButtonStyled>;
 };
